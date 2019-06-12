f74119204b7446f2b3f73c0c8ce6a967a41e7c88
annprog <tecbbs@qq.com>
2019-06-12 12:47:31 +0800

    fix init.sh 正确生成内容

--- init.sh
+++ init.sh
@@ -1,35 +1,89 @@
 #!/bin/bash
-test -d src && echo "already init" && exit 1 || mkdir src
-test -d tmp || mkdir tmp
+
+DIR=`cd $(dirname $0);pwd`
+SRC="$DIR/src"
+TMP="$DIR/tmp"
+REVISION="$DIR/REVISION.txt"
+DIFF="$DIR/diff"
+
+function mkDir() {
+	test -d $1 || mkdir $1
+}
+
+mkDir $TMP
+mkDir $DIFF
+
 cd tmp
 test -d gobyexample || git clone https://github.com/mmcgrana/gobyexample
-cd gobyexample
+cd gobyexample && git pull
 
-REVISION=`git rev-parse HEAD`
-echo $REVISION > ../../REVISION.txt
+REV=`git rev-parse --short HEAD`
+if [ -d $SRC ];then
+	[ ! -f $REVISION ] && echo "$REVISION lost. Please re init" && exit 1
+	OLDREV=`cat $REVISION`
+	echo "already init."
+	echo -e "see $DIFF/$REV-$OLDREV.diff\n\n"
+	git diff HEAD $OLDREV  -- . ':!public' > $DIFF/$REV-$OLDREV.diff
+	exit 
+else
+	mkDir $SRC
+	echo $REV > $REVISION
+fi
 
 n=10000
 
-function parse() {
-	echo "::: {.en}" >> $md
-	s="examples/$DIR/$DIR.$2"
-	grep "$1 " $s |sed -r "s|[ ]*$1 ||g">> $md
-	echo -e ":::\n\n" >> $md
-	
-	echo -e "::: {.zh}\n\n:::\n\n" >> $md
-	
-	echo "\`\`\`$2" >> $md
-	grep -v "$1 " $s >> $md
-	echo -e "\`\`\`\n\n" >> $md	
+function checkType() {
+	echo "$1" |grep "^#" &>/dev/null && echo "comment" && return 0
+	echo "$1" |grep -E "^\ *//\ " &>/dev/null && echo "comment" && return 0
+	echo "code" && return 0
 }
 
+function parse() {
+	lang=$1
+	s="$TMP/gobyexample/examples/$2/$2.$lang"
+	markdown=$3
+	comment=""
+	code=""
+	first=`head -n 1 $s`
+	current=`checkType "$first"`
+	while read c;do
+		if [ $current == "comment" ];then
+			if [ `checkType "$c"` == "comment" ];then
+				comment="$comment\n`echo $c |sed -r 's|^\ *//\ ||g'|sed -r 's/^# //g'`"
+			else
+				current="code"
+				echo -e "::: {.en}$comment\n:::\n\n::: {.zh}\n\n:::\n\n" >> $markdown
+				comment=""
+				code="$code\n$c"
+			fi
+		elif [ $current == "code" ];then
+			if [ `checkType "$c"` == "code" ];then
+				code="$code\n$c"
+			else
+				current="comment"
+				echo -e "\`\`\`$lang$code\n\`\`\`\n\n" >> $markdown
+				code=""
+				comment="$comment\n`echo $c |sed -r 's|^\ *//\ ||g'|sed -r 's/^# //g'`"
+			fi
+		fi
+	done < $s
+	[ "$comment"x != ""x ] && echo -e "::: {.en}$comment\n:::\n\n::: {.zh}\n\n:::\n\n" >> $markdown
+	[ "$code"x != ""x ] && echo -e "\`\`\`$lang$code\n\`\`\`\n\n" >> $markdown
+}
+
+if [ "$1"x == "-d"x ];then
+	parse go $2 "$SRC/debug.md"
+	parse sh $2 "$SRC/debug.md"
+	exit 0
+fi
+
 while read line;do
-	DIR=`echo $line |tr -d "'"|tr ' /' '-' |tr '[A-Z]' '[a-z]' |sed -r 's/-+/-/g'`
-	md="../../src/$n-$DIR.md"
-	echo -e "\n# $line {.en}\n\n" > $md
+	GODIR=`echo $line |tr -d "'"|tr ' /' '-' |tr '[A-Z]' '[a-z]' |sed -r 's/-+/-/g'`
+	md="$SRC/$n-$GODIR.md"
+	echo -e "\n# $line {.en}\n" > $md
 	echo -e "\n# $line {.zh}\n\n" >> $md
-	
-	parse "//" go
-	parse "#" sh
+	echo "init $GODIR"
+	parse go $GODIR $md
+	parse sh $GODIR $md
 	((n=n+1))
 done < examples.txt
